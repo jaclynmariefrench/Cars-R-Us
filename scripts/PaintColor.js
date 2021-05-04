581b85d96736c0a7be68a7ff803bea714626fc6a
Jaclyn French <jaclynmariefrench@gmail.com>
2021-05-04 16:00:10 -0500

    stoped in order module

--- scripts/PaintColor.js
+++ scripts/PaintColor.js
@@ -1,5 +1,14 @@
 import { getPaintColor } from "./database.js";
 
+document.addEventListener("click", (event) => {
+    const itemClicked = clickEvent.target
+    if(itemClicked.id.startsWith("paint")) {
+        const [, paintId] = itemClicked.id.split("--")
+        window.alert(`Hi`)
+    }
+    
+    })
+
 
 const paintColor = getPaintColor()
 
@@ -8,7 +17,7 @@ export const Paint = () => {
 
     const listItemArray = paintColor.map((paint) => {
         return `<div>
-                    <input type="radio" name="paint" value="${paint.id}"/>${paint.type} ${paint.price}
+                    <input type="radio" name="paint--" value="${paint.id}"/>${paint.type} ${paint.price}
         </div>`
     });
     
--- scripts/orders.js
+++ scripts/orders.js
@@ -1,10 +1,14 @@
 import { getOrders } from "./database.js"
 
+const buildOrderListItems = (order) => {
+// PUT IN MORE HERE 5/5
+}
+
 export const Orders = () => {
     const orders = getOrders();
     let html = "<ul>";
     // PUT IN FUNCTION HERE
-    const listItems = orders.map();
+    const listItems = orders.map(buildOrderListItems);
     html += listItems.join("");
     html += "</ul>";
     return html
